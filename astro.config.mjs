f83415c99ac0e6b0035a38af5ed25e47d36c3cf4
knnv-ar <106931704+knnv-ar@users.noreply.github.com>
2024-04-08 14:12:49 -0300

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -64,7 +64,7 @@ export default defineConfig({
 						},
 						{
 							label: 'Clase 6',
-							collapsed: true,
+							collapsed: false,
 							autogenerate: { directory: 'clases/clase-6' },
 						},
 						{
